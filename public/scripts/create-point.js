40c51534b42d6b056ddb226bd74d06fe57aa0b3a
Gabrielolliverl <48062591+Gabrielolliverl@users.noreply.github.com>
2020-06-06 16:20:27 -0300

    Finalizando

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,83 +1,104 @@
-// adicionar uma api de estados e cidades
 function populateUFs() {
-   const ufSelect = document.querySelector('select[name=uf]')
+    const ufSelect = document.querySelector("select[name=uf]")
 
-   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-   .then( res => res.json() )
-   .then( states => {
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
 
-      for ( state  of states ) {
-         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-         
-      }
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
 
-   })
+    })
 }
 
 populateUFs()
 
+
 function getCities(event) {
-   const citySelect = document.querySelector("[name=city]")
-   const stateInput = document.querySelector("[name=state]")
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
 
-   const ufValue = event.target.value
+    const ufValue = event.target.value
 
-   const indexOfSelectedState = event.target.selectedIndex
-   stateInput.value = event.target.options[indexOfSelectedState].text
-
-   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-   citySelect.innerHTML = `<option value>Selecione a Cidade</option>`
-   citySelect.disabled = true
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
 
-   fetch(url)
-      .then( res => res.json() )
-      .then( cities => {
-         for ( city of cities ) {
-            citySelect.innerHTML +=  `<option value="${city.nome}">${city.nome}</option>`
-         }
-         citySelect.disabled = false
-      })
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        
+
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
 }
+
 
 document
-            .querySelector('select[name=uf]')
-            .addEventListener('change', getCities)
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
 
-// Selecionar os li do fieldset items de coleta
-// Pegar todos os li
-const itemsToCollect = document.querySelectorAll('.items-grid li')
 
-for ( item of itemsToCollect ) {
-   item.addEventListener('click', handleSelectedItem)
+// Itens de coleta
+// pegar todos os li`s
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
 }
 
-const collectedItems = document.querySelector('[name=items]')
 
-let seletctedItems = []
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
 
 function handleSelectedItem(event) {
-   const itemLi = event.target
-   
-   itemLi.classList.toggle('selected')
+    const itemLi = event.target
 
-   const itemId = itemLi.dataset.id
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
 
-   // Verificar se existem itens selecionados, se sim
-   // Pegar esses itens selecionados
-   const alreadySelected = seletctedItems.findIndex( item => item == itemId ) // isso será true ou false
-   // Se ja estiver selecionado, tirar da seleção
-   if( alreadySelected != -1 ) {
-      const filteredItems = seletctedItems.filter( item => item != itemId) // false
-      seletctedItems = filteredItems
-   }else {
-      // Se não estiver selecionado, adicionar a seleção
-       seletctedItems.push(itemId)
+    // console.log('ITEM ID: ', itemId)
+
+    // verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    // se já estiver selecionado, 
+    if( alreadySelected >= 0 ) {
+        // tirar da selecao
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
     }
 
-    console.log(seletctedItems);
-   // Atualizar o campo escondido com os itens selecionados
-   // document.querySelector('input[name=items]')
-   collectedItems.value = seletctedItems
+    // console.log('selectedItems: ', selectedItems)
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
 }
--- src/database/db.js
+++ src/database/db.js
@@ -1,40 +1,82 @@
+// importar a dependencia do sqlite3
 const sqlite3 = require("sqlite3").verbose()
 
-// criar o objeto que irá fazer operações no banco de dados 
+// criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
-           
-// utilizar o objeto de banco de dados, para  as nossas operações
 
-db.serialize( () => {
+module.exports = db
+// utilizar o objeto de banco de dados, para nossas operações
+db.serialize(() => {
 
-    //criar tabela
-    // criar tabelas com comandos sql
+    // com commandos SQL eu vou:
 
-    db.run(`
+    // // 1 Criar uma tabela 
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS places (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         image TEXT,
+    //         name TEXT,
+    //         address TEXT,
+    //         address2 TEXT,
+    //         state TEXT,
+    //         city TEXT,
+    //         items TEXT
+    //     );
+    // `)
 
-        CREATE TABLE IF NOT EXISTS places (
+    // // 2 Inserir dados na tabela
+    // const query = `
+    //     INSERT INTO places (
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) VALUES (?,?,?,?,?,?,?);
+    // `
 
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT 
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    //     "Papersider",
+    //     "Guilherme Gemballa, Jardim América",
+    //     "Número 260",
+    //     "Santa Catarina",
+    //     "Rio do Sul",
+    //     "Resíduos Eletrônicos, Lâmpadas"
+    // ]
+
+    // function afterInsertData(err) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Cadastrado com sucesso")
+    //     console.log(this)
+    // }
+
+    // db.run(query, values, afterInsertData)
 
 
-        );
+    // 3 Consultar os dados da tabela
+    // db.all(`SELECT name FROM places`, function(err, rows) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
 
+    //     console.log("Aqui estão seus registros: ")
+    //     console.log(rows)
+    // })
 
+    // 4 Deletar um dado da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [3], function(err) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
 
-    
-    
-    `)
+    //     console.log("Registro deletado com sucesso!")
+    // })
 
-    // inserir dados da tabela
-
-    //consultar os dados da tabela
-
-    //deletar um dado da tabela
-     
 })
+
