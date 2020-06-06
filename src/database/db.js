b19c734649f1edb6c3e64db0052a7a7f81112179
Gabrielolliverl <48062591+Gabrielolliverl@users.noreply.github.com>
2020-06-06 14:16:13 -0300

    Criando DB

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,40 @@
-/// importa a dependencia do sqlite 3
+const sqlite3 = require("sqlite3").verbose()
 
-const sqlite3 = require("sqlite3")
+// criar o objeto que irá fazer operações no banco de dados 
+const db = new sqlite3.Database("./src/database/database.db")
+           
+// utilizar o objeto de banco de dados, para  as nossas operações
+
+db.serialize( () => {
+
+    //criar tabela
+    // criar tabelas com comandos sql
+
+    db.run(`
+
+        CREATE TABLE IF NOT EXISTS places (
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT 
 
 
+        );
+
+
+
+    
+    
+    `)
+
+    // inserir dados da tabela
+
+    //consultar os dados da tabela
+
+    //deletar um dado da tabela
+     
+})
